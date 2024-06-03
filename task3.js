dd9d8fe459f6d6c634321ed2b249636935825b49
Andrii Dobronos <andriidobronos@gmail.com>
2024-06-03 16:08:25 +0300

    resolve new tasks

--- task3.js
+++ task3.js
@@ -84,3 +84,15 @@ console.log(score( [1, 1 ,2, 3, 6] )) //0
 //console.log(score( [4, 4, 4, 1, 3] )) //500
 //console.log(score([5, 5, 3, 3, 5]))//500
 //console.log(score([1,4,4,5,4]))//400
+//*************************************************************//
+// best solution :
+
+function _score( dice ) {
+    const dc = [0,0,0,0,0,0];
+    const tdr = [1000,200,300,400,500,600];
+    const sdr = [100,0,0,0,50,0];
+    dice.forEach(function(x){ dc[x-1]++; });
+    return dc.reduce(function(s,x,i){
+        return s + (x >= 3? tdr[i] : 0) + sdr[i]*(x % 3);
+    },0);
+}
